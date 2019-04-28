bbcb4929323e7addf979e78e6d7b38c0660df563
Ishan Mohanty <ishanwiz1991@gmail.com>
2019-04-27 22:08:04 -0700

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='gym_LunarOrbiter',
-      version='0.01',
-      url='https://github.com/IshanMohanty/gym-LunarOrbiter',
-      description='Lunar Orbiter',
-      install_requires=['gym','box2d-py','numpy']
-)
