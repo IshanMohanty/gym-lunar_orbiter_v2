a6298fad5cf52fa2decfb864c7df27d174f771ea
Ishan Mohanty <ishanwiz1991@gmail.com>
2019-04-27 22:06:55 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gym_LunarOrbiter',
+      version='0.01',
+      url='https://github.com/IshanMohanty/gym-LunarOrbiter',
+      description='Lunar Orbiter',
+      install_requires=['gym','box2d-py','numpy']
+)
