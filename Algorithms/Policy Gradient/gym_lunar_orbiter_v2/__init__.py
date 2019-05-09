77d8be1aaff77c2bc7c37d7e9c4d63259455b4c6
Ishan Mohanty <ishanwiz1991@gmail.com>
2019-05-09 15:55:24 -0700

    Delete __init__.py

--- Algorithms/Policy Gradient/gym_lunar_orbiter_v2/__init__.py
+++
@@ -1,8 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='LunarOrbiter-v2',
-    entry_point='gym_lunar_orbiter_v2.envs:LunarOrbiterV2',
-    max_episode_steps=1000,
-    reward_threshold=200,
-)
